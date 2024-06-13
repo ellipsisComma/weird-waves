2b529b3b734fd4e70feccc2b33bb3473f6dec25e
01bbl <01bbl@noreply.codeberg.org>
2024-06-13 00:44:27 +0000

    Replaced all string literals with template literals

--- initialisation.js
+++ initialisation.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 11*/
 
 // initialise user-selected (or default) site display settings
-const styles = JSON.parse(window.localStorage.getItem("styles")) ?? {};
-styles.theme ??= "dark";
-styles.font ??= "serif";
+const styles = JSON.parse(window.localStorage.getItem(`styles`)) ?? {};
+styles.theme ??= `dark`;
+styles.font ??= `serif`;
 for (const [style, option] of Object.entries(styles)) document.body.dataset[style] = option;
