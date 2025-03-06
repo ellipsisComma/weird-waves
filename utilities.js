f05dd6658408ae81ae1891863352ad789153205b
ellipsisComma <no.reply@ellipsiscomma.com>
2025-03-06 18:30:23 +0000

    added string hash utility

--- utilities.js
+++ utilities.js
@@ -139,6 +139,26 @@ function getJaroSimilarity(str1, str2) {
 	return similarity;
 }
 
+// fast, modern string hash
+// cyrb53 (c) 2018 bryc (github.com/bryc)
+String.prototype.hashCode = function (seed = 0) {
+	let h1 = 0xdeadbeef ^ seed;
+	let h2 = 0x41c6ce57 ^ seed;
+
+	for (let i = 0, ch; i < this.length; i++) {
+		ch = this.charCodeAt(i);
+		h1 = Math.imul(h1 ^ ch, 2654435761);
+		h2 = Math.imul(h2 ^ ch, 1597334677);
+	}
+
+	h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+	h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+	h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+	h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+	
+	return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
 
 
 /* =========
