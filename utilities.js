002762c1d31053f9fe6569c1b409010b647415b4
ellipsisComma <no.reply@ellipsiscomma.com>
2025-04-08 22:58:37 +0100

    tweaked localStorageGet() error message

--- utilities.js
+++ utilities.js
@@ -238,7 +238,7 @@ function localStorageGet(key, defaultValue) {
 	try {
 		return JSON.parse(localStorage.getItem(key)) ?? defaultValue;
 	} catch {
-		console.error(`failed to retrieve stored value for key "${key}" correctly (either no value was stored, or the value was empty) so returned default value instead`);
+		console.error(`Failed to retrieve stored value for key "${key}" correctly (either no value was stored, or the value was empty) so returned default value instead.`);
 		return defaultValue;
 	}
 }
