9f0c9f2dd7c155eb85a41ad0d54524685325fe5f
ellipsisComma <no.reply@ellipsiscomma.com>
2025-07-10 11:28:26 +0100

    stop Flash of Unthemed Content in (older versions of?) Safari by setting theme transition to 0s initially and updating to 1s on DOMContentLoaded

--- initialisation.js
+++ initialisation.js
@@ -4,3 +4,11 @@
 for (const [style, option] of Object.entries(localStorageGet(`styles`) ?? {})) {
 	document.documentElement.dataset[style] = option;
 }
+
+/*
+	for Safari: theme transition duration must start at 0 (in stylesheet)
+	and be set to 1s later (in at least some older versions)
+*/
+document.addEventListener(`DOMContentLoaded`, () =>
+	document.documentElement.style.setProperty(`transition-duration`, `1s`)
+);
